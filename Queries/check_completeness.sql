913d0f626ae30d18e9b87b47f8e796e697d4744c
Steven Tran <145170256+SteveT2023@users.noreply.github.com>
2026-09-15 14:02:37 -0400

    UPLOAD | check_completeness.sql

---
+++ Queries/check_completeness.sql
@@ -0,0 +1,44 @@
+-- This file will show the process of checking for completeness.
+
+-- Create a procedure to count for nulls, blanks, and whitespaces.
+DELIMITER //
+	CREATE PROCEDURE check_completeness(col_name VARCHAR(100))
+		BEGIN
+			SET @query = CONCAT(
+				'INSERT INTO result_completeness ',
+                'SELECT ',
+					'"', col_name, '" AS column_name, ',
+					'SUM(CASE WHEN ', col_name, ' IS NULL THEN 1 ELSE 0 END) AS null_count, ',
+					'SUM(CASE WHEN ', col_name, ' = "" THEN 1 ELSE 0 END) AS blank_count, ',
+					'SUM(CASE WHEN TRIM(', col_name, ') = "" AND ', col_name, ' != "" THEN 1 ELSE 0 END) AS whitespace_count ',
+                'FROM raw_cafe'
+            );
+            
+            PREPARE stmt FROM @query;
+            EXECUTE stmt;
+            DEALLOCATE PREPARE stmt;
+		END //
+DELIMITER ;
+
+-- Create a temporary table to display result.
+CREATE TEMPORARY TABLE result_completeness
+(
+	column_name VARCHAR(100),
+    null_count VARCHAR(100),
+    blank_count VARCHAR(100),
+    whitespace_count VARCHAR(100)
+);
+
+-- Call procedure.
+CALL check_completeness('transaction_id');
+CALL check_completeness('item');
+CALL check_completeness('quantity');
+CALL check_completeness('price_per_unit');
+CALL check_completeness('total_spent');
+CALL check_completeness('payment_method');
+CALL check_completeness('location');
+CALL check_completeness('transaction_date');
+
+-- Display result
+SELECT *
+FROM result_completeness;
