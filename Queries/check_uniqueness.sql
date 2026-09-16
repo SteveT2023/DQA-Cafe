98ea5c489ffbdec1971994c5817528eb2ed30130
Steven Tran <145170256+SteveT2023@users.noreply.github.com>
2026-09-16 12:18:39 -0400

    UPDATE | check_uniqueness.sql

--- Queries/check_uniqueness.sql
+++ Queries/check_uniqueness.sql
@@ -5,13 +5,3 @@ SELECT transaction_id, COUNT(*) AS dup_count
 FROM raw_cafe
 GROUP BY transaction_id
 HAVING COUNT(*) >= 2;
-
--- Since there is no duplicates, display the result for evidence.
-SELECT COUNT(*) AS dup_group_of_values
-FROM
-(
-	SELECT transaction_id, COUNT(*)
-	FROM raw_cafe
-	GROUP BY transaction_id
-	HAVING COUNT(*) >= 2
-) AS q;
